7ec157dfff5a941b64e23163c8cd30f0e0009cfe
alirezafarahaniii <a.farahani@shooka.ir>
2023-10-12 02:11:05 +0330

    server1 log file added

--- weather-project/server1/src/main/java/com/example/server1/controller/MessageController.java
+++ weather-project/server1/src/main/java/com/example/server1/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.example.server1.controller;
 
 import com.example.server1.service.RabbitMQProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class MessageController {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
     private RabbitMQProducer producer;
 
     public MessageController(RabbitMQProducer producer) {
@@ -20,6 +22,7 @@ public class MessageController {
     // http://localhost:8080/api/v1/publish?message=hello
     @GetMapping("/publish")
     public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+        LOGGER.info(String.format("Received message -> %s", message));
         producer.sendMessage(message);
         return ResponseEntity.ok("Message sent to RabbitMQ ...");
     }
